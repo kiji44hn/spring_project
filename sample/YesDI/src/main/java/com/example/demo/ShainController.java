2ecf8afa9c067e15fa3182fe84e2d303f1390146
kijihn22 <dekillji44hn@yahoo.co.jp>
2024-10-23 14:35:40 +0900

    Start
    Start

---
+++ sample/YesDI/src/main/java/com/example/demo/ShainController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ShainController {
+	
+	private final ShainService shainService;
+
+	//ShainServiceのDI
+	public ShainController(ShainService shainService) {
+	this.shainService = shainService;
+	}
+
+	@RequestMapping("/input")
+	public String index(ShainForm shainform) {
+		return "index.html";
+	}
+
+	@RequestMapping("/output")
+	public String result(@Validated ShainForm shainForm,
+			BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "index.html";
+		}
+
+		//String name = "コントローラー太郎";
+		//サービス層から社員を検索
+		String name = shainService.findByNo(shainForm.getNumber());
+		
+		model.addAttribute("number", shainForm.getNumber());
+		model.addAttribute("name", name);
+		return "output.html";
+	}
+
+}
